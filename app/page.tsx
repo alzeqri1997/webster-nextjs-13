7f4dd7481cc2155923bfb84ccf630abdeb0907a0
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2023-06-17 19:32:36 +0300

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+import Hero from 'public/hero.png'
+
 
 export default function Home() {
   return (
@@ -8,6 +11,14 @@ export default function Home() {
         <h1 className={styles.title}>
           Better design for your digital products.
         </h1>
+        <p className={styles.desc} >
+          Turing your Idea into Reality. We bring together the teams from the global tech industry.
+        </p>
+        <Button url="/portfolio" text="See Our Works" />
+      </div>
+
+      <div className={styles.item} >
+        <Image src={Hero} alt="hero section" className={styles.img} />
       </div>
     </div>
   );
