7217c3c0e3e00fc48b24621fd14e98031e7603b1
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2023-06-17 11:27:34 +0300

    setup post api

---
+++ app/api/posts/route.ts
@@ -0,0 +1,19 @@
+import Post from "@/models/Post";
+import connect from "@/utils/db"
+import {NextResponse} from 'next/server'
+
+
+
+export const GET = async (request: Request)=>{
+
+    try {
+        await connect();
+
+        const posts = await Post.find();
+
+
+        return new NextResponse(JSON.stringify(posts), {status: 200})
+    } catch (error) {
+        return new NextResponse("Something went wrong", {status: 500})
+    }
+}
