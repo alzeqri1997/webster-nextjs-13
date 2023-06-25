d84f5cf6e581645e2b8b11084c0f12bdd54aea4e
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2023-06-25 17:15:00 +0300

    responsive tablet

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,11 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.css";
 import Link from "next/link";
-import { Session } from "inspector";
 import { signIn, signOut, useSession } from "next-auth/react";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
-// import { signOut, useSession } from "next-auth/react";
 const links = [
   {
     id: 1,
@@ -41,26 +39,25 @@ const links = [
 
 const Navbar = () => {
   const session = useSession();
+  const [showMenu, setShowMenu] = useState<boolean>(false)
   return (
     <div className={styles.container}>
       <Link href={"/"} className={styles.logo}>
         Webster
       </Link>
+      <button className={styles.button}>
+        <svg  width="18" height="14" viewBox="0 0 18 14" aria-hidden="true" data-type="type-1"> <rect fill="currentColor" y="0.00" width="18" height="1.7" rx="1"></rect> <rect fill="currentColor" y="6.15" width="18" height="1.7" rx="1"></rect> <rect fill="currentColor" y="12.3" width="18" height="1.7" rx="1"></rect> </svg>
+      </button>
       <div className={styles.links}>
-        <DarkModeToggle/>
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
-        {/* {session.status === "authenticated" && (
-            <button className={styles.logout} onClick={() => signOut()}>
-              LogOut
-            </button>
-          )} */}
-          {session.status === "authenticated" ? 
-            <button onClick={ ()=> signOut() } className={styles.logout}>LogOut</button> :
-            <button onClick={ ()=> signIn()  } className={styles.logout}>login</button> 
+        {session.status === "authenticated" ?
+          <button onClick={() => signOut()} className={styles.logout}>LogOut</button> :
+          <button onClick={() => signIn()} className={styles.logout}>login</button>
         }
       </div>
     </div>
